8c67a75961a13338870171b4b0b4f2cfb2859b38
Abdulrahman Nasser <abdulrahman.nasser222@gmail.com>
2025-03-21 14:21:48 +0200

    Add the database and style the 2 pages

--- app.py
+++ app.py
@@ -1,26 +1,37 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'dwdwjijdwid jwidjwi jiwjd iwjdiwjd'
 
-tasks_list = []
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
 
+db = SQLAlchemy(app)
 
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
 
-@app.route('/',methods=['POST','GET'])
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+
+@app.route('/', methods=['POST', 'GET'])
 def add_task():
     if request.method == 'POST':
-        task = request.form.get('task')
-        completed = 'completed' in request.form 
+        task_content = request.form.get('task')
+        completed = 'completed' in request.form
         
-        tasks_list.append({'task': task, 'completed': completed})
+        current = Task(task=task_content, completed=completed)
+        db.session.add(current)
+        db.session.commit()
         return redirect(url_for('tasks'))
-    return render_template('add_task.html', title='Add Task', tasks= tasks_list)
+    return render_template('add_task.html', title='Add Task')
 
-@app.route('/tasks',methods=['POST','GET'])
+@app.route('/tasks', methods=['POST', 'GET'])
 def tasks():
-    return render_template('tasks.html', title='Tasks', tasks= tasks_list)
-
+    tasks_list = Task.query.all()
+    return render_template('tasks.html', title='Tasks', tasks_list=tasks_list)
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
